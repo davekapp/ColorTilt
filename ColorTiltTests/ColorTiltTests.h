c8263a4f8194e7d59305bdbf500a33e93a0abdb6
David Kapp <davekapp@gmail.com>
2012-02-15 20:10:40 -0700

    Initial Commit

---
+++ ColorTiltTests/ColorTiltTests.h
@@ -0,0 +1,13 @@
+//
+//  ColorTiltTests.h
+//  ColorTiltTests
+//
+//  Created by David Kapp on 2/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ColorTiltTests : SenTestCase
+
+@end
---
+++ ColorTiltTests/ColorTiltTests.m
@@ -0,0 +1,32 @@
+//
+//  ColorTiltTests.m
+//  ColorTiltTests
+//
+//  Created by David Kapp on 2/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ColorTiltTests.h"
+
+@implementation ColorTiltTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ColorTiltTests");
+}
+
+@end
